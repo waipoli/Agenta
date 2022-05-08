252422bea57d8049ad8b029c6bddd81d468514d7
ElMishucha <Mishutka.Bolshakov@gmail.com>
2022-05-08 16:33:06 +0300

    Submit

--- src/app/pages/game-page/components/submit/submit.component.ts
+++ src/app/pages/game-page/components/submit/submit.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmitComponent implements OnInit {
 
+  fileSelected = "No file selected";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFileSelected(evt: any): void {
+    this.fileSelected = evt.target.files[0].name;
+  }
 }
