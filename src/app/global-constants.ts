6c982e7e3153e75feb2338616d99d69ffe07f0ea
maxim <maximschved8@gmail.com>
2022-07-10 19:40:14 +0200

    Add shadow

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,5 +1,5 @@
 export class GlobalConstants {
-  public static title: string = "AGENTA";
+  public static title: string = "BotCoder";
   public static countOfNewsPerPage = 10;
   public static serverUrl = "https://localhost:5001/"
 }
