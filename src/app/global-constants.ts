92b4ff708b9f5acd29920fbcb4756c8848fa9f26
maxim <maximschved8@gmail.com>
2022-08-02 18:21:50 +0200

    serve1

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
   public static title: string = "BotCoder";
   public static countOfNewsPerPage = 10;
-  public static serverUrl = "https://bf86-2a01-598-b9ae-bec-6481-97bb-b21b-f834.eu.ngrok.io"
+  public static serverUrl = "https://bf86-2a01-598-b9ae-bec-6481-97bb-b21b-f834.eu.ngrok.io/"
   public static salt = "I0u6yFaSTQyX724"
 }
